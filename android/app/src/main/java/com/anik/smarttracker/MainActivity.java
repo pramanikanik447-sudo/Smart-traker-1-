76ca8c74240112f7bcd5c161823b16616d469693
Anik Pramanik <pramanikanik447@gmail.com>
2026-07-20 10:49:50 +0530

    Add Capacitor Android

---
+++ android/app/src/main/java/com/anik/smarttracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.anik.smarttracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.anik.smarttracker',
+  appName: 'smart tracker',
+  webDir: 'dist'
+};
+
+export default config;
